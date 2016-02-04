38d3a24d2022f3961c685129d7b9796e6f6a3e0a
Doni Ramadhan <doniramadhan@gmail.com>
2016-02-04 12:11:45 +0100

    Initial Commit

---
+++ Cello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cello
+//
+//  Created by Doni Ramadhan on 04/02/16.
+//  Copyright © 2016 Cello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
