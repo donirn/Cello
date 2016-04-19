69dc3851a34b5c91f6733ed82b150464dee9c7ae
Doni Ramadhan <doniramadhan@gmail.com>
2016-04-19 10:56:55 +0200

    Rename ViewController to HomeViewController

--- Cello/HomeViewController.swift
+++ Cello/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Cello
 //
 //  Created by Doni Ramadhan on 04/02/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
